802245bf407cd02a8369775dc843ff7c6be4fd86
regularcoder123 <atharva.78722@gmail.com>
2025-10-23 16:24:06 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let count = 0;
+let totalNumbers = 10; 
+
+while (count < totalNumbers) {
+  let randomNumber = Math.floor(Math.random() * 10000000000000001);
+  
+
+  console.log(randomNumber);
+  
+
+  count++;
+}
